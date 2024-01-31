1d1d3df8868c89a1e98443899e985b524267d712
jakupwhansen <jakupwhansen@hotmail.com>
2024-01-31 10:03:11 +0100

    Add project files.

---
+++ KrydsOgBolle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrydsOgBolleCore
+{
+    public class KrydsOgBolle
+    {
+        public List<int> listenAfxogy = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        public int aktivPlayer = 1; //1 eller 2 er aktive.
+
+        public bool move(int pos)
+        {
+            bool svar = false;
+            if (listenAfxogy[pos]==0)
+            {
+                listenAfxogy[pos] = aktivPlayer;
+                //---Ændrer spiller--------------
+                if (aktivPlayer == 1)
+                    aktivPlayer = 2;
+                else
+                    aktivPlayer = 1;
+                //------------------------------
+                svar = true;
+            }
+            return svar;
+        }
+        public List<int> getGame()
+        {
+            return listenAfxogy;
+        }
+        public void resetGame()
+        {
+            listenAfxogy = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using KrydsOgBolleCore;
+
+KrydsOgBolle kb = new KrydsOgBolle();
+
+while (true)
+{
+    Console.Clear();
+    List<int> p = kb.getGame();
+    Console.WriteLine(p[0] + " | " + p[1] + " | " + p[2]);
+    Console.WriteLine("---------");
+    Console.WriteLine(p[3] + " | " + p[4] + " | " + p[5]);
+    Console.WriteLine("---------");
+    Console.WriteLine(p[6] + " | " + p[7] + " | " + p[8]);
+    Console.WriteLine("   ");
+    Console.WriteLine("Hvilken plads vil du sætte nu?");
+    String ind = Console.ReadLine();
+    try
+    {
+        int i = Convert.ToInt32(ind);
+        kb.move(i);
+    }
+    catch (Exception ex) { }
+}
