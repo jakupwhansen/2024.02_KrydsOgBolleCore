8271a7e9b66e32dc8631f7b7102d41cde959b8b5
jakupwhansen <jakupwhansen@hotmail.com>
2024-01-31 10:15:18 +0100

    Udvidet så at 1 og 2 bliver til X og O

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 ﻿using KrydsOgBolleCore;
 
 KrydsOgBolle kb = new KrydsOgBolle();
-
+IDictionary<int, string> xo = new Dictionary<int, string>();
+xo.Add(0, " ");
+xo.Add(1, "X"); //adding a key/value using the Add() method
+xo.Add(2, "O");
 while (true)
 {
     Console.Clear();
     List<int> p = kb.getGame();
-    Console.WriteLine(p[0] + " | " + p[1] + " | " + p[2]);
+    Console.WriteLine(xo[p[0]] + " | " + xo[p[1]] + " | " + xo[p[2]]);
     Console.WriteLine("---------");
-    Console.WriteLine(p[3] + " | " + p[4] + " | " + p[5]);
+    Console.WriteLine(xo[p[3]] + " | " + xo[p[4]] + " | " + xo[p[5]]);
     Console.WriteLine("---------");
-    Console.WriteLine(p[6] + " | " + p[7] + " | " + p[8]);
+    Console.WriteLine(xo[p[6]] + " | " + xo[p[7]] + " | " + xo[p[8]]);
     Console.WriteLine("   ");
     Console.WriteLine("Hvilken plads vil du sætte nu?");
     String ind = Console.ReadLine();
